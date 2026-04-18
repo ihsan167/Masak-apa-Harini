09c9db01a3e018c468cccd8e93b4d1560548bfc0
Ihsan <ihsan167@gmail.com>
2026-04-18 16:23:11 +0800

    Initial commit: blank Expo app setup

---
+++ mobile/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Boleh masak apa harini</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7b60ba',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
